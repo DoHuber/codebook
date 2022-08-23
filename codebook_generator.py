6ae773c70d1d2c618cb6eeff6f06886da7b8d56a
Dominik Huber <dominik@huber-swe.de>
2022-08-23 23:06:00 +0200

    Initial version, forgot to push

--- codebook_generator.py
+++ codebook_generator.py
@@ -1,7 +1,7 @@
 
 
 # Min and max are inclusive
-def get_true_random_numbers(amount, min, max):
+def get_true_random_numbers(amount, min, max) -> list:
     numbers = []
     while len(numbers) < amount:
         # Get fresh randomness and process
@@ -19,8 +19,53 @@ def get_true_random_numbers(amount, min, max):
     return numbers
 
 
+def generate_code_page():
+    # Each page contains 150 random numbers
+    random_nos = get_true_random_numbers(150, 0, 35)
+    blocks = []
+
+    list_index = 0
+    for i in range(10):
+        current_numbers = []
+        for j in range(15):
+            current_numbers.append(random_nos[list_index])
+            list_index += 1
+
+        current_numbers = [str(x) for x in current_numbers]
+        block = "\t" + " & ".join(current_numbers) + " \\\\"
+        block = block + """
+        \\U & \\U & \\U & \\U & \\U & \\U & \\U & \\U & \\U & \\U & \\U & \\U & \\U & \\U & \\U \\\\
+        \\\\
+        """
+
+        blocks.append(block)
+
+    blocks = "\\hline \\\\\n".join(blocks)
+
+    page = f"""
+    \\begin{{flushleft}}
+    \\begin{{tabular}}{{c c c c c @{{\\hskip 2em}} c c c c c @{{\\hskip 2em}} c c c c c}}
+    {blocks}
+    \\end{{tabular}}
+    \\end{{flushleft}}
+    \\newpage
+    """
+
+    return page
+
+
 if __name__ == "__main__":
-    numbers = get_true_random_numbers(256, 0, 35)
-    print(numbers)
-    print(f"Length/Min/Max: {len(numbers)}/{min(numbers)}/{max(numbers)}")
-    print(f"Average: {sum(numbers)/len(numbers)}")
+    number_of_pages = 20
+    pages = []
+    for i in range(number_of_pages):
+        pages.append(generate_code_page())
+        print(f"Generated {i+1}/{number_of_pages} code pages.")
+
+    with open("codebook-template.txt", "r") as infile:
+        template = infile.read()
+
+    pages_str = "\n".join(pages)
+    finished_pages = template.replace("<pageshere>", pages_str)
+
+    with open("codepages.tex", "w") as outfile:
+        outfile.write(finished_pages)
